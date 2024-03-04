c4c67683d121f183952d6986b68fb983ad599632
Nick <nodhiambo01@gmail.com>
2024-03-04 14:41:22 +0300

    Create data model

---
+++ manager/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class PasswordEntry(models.Model):
+    """Defines password's data structure"""
+    website_url = models.URLField(max_length=200)
+    username = models.CharField(max_length=100)
+    encrypted_password = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """Returns a string rep of password entry"""
+        return f"{self.username} - {self.website_url}"
