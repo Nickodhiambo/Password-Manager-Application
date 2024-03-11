a934011a1f0673b602a293bbd67009a464b5280f
WinnyKay3 <winffredkalondu@gmail.com>
2024-03-11 12:31:42 +0300

    update changes

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('manager/', include('manager.urls')),
-        path('users/', include('users.urls'))
+    path('admin/', admin.site.urls),
+    path('manager/', include('manager.urls')),
+    path('users/', include('users.urls')),
+    path('', RedirectView.as_view(url='/manager/index/', permanent=True)),  # Redirect root URL to /manager/index/
 ]
+
