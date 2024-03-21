be08521150ca0c18aa052822fe4c514f7e22618f
WinnyKay3 <winffredkalondu@gmail.com>
2024-03-21 17:49:13 +0300

    Add more changes

--- static/script.js
+++ static/script.js
@@ -1,30 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Copy functionality
-  var copyIcon = document.querySelectorAll('.copy');
-  copyIcon.forEach((icon) => {
-    icon.addEventListener('click', function(event) {
-        const input = event.target.parentNode.querySelector('input')
-        navigator.clipboard.writeText(input.value).then(function() {
-            event.target.innerText = "check"
-            setTimeout(() => {
-                event.target.innerText = 'content_copy'
-            }, 1000)
-        }, function(err) {
-            console.error('Could not copy text: ', err);
-        });
+const generatePasswordBtn = document.getElementById('generatePasswordBtn');
+const passwordInput = document.getElementById('id_password');
+if(generatePasswordBtn) {
+    generatePasswordBtn.addEventListener('click', function () {
+        const newPassword = generateStrongPassword();
+        passwordInput.value = newPassword;
     });
-  })
+    
+}
 
-  // Toggle password visibility
-  var toggleVisibilityIcon = document.getElementById('toggleVisibility');
-  toggleVisibilityIcon.addEventListener('click', function() {
-      var passwordInput = document.getElementById('passwordInput');
-      if (passwordInput.type === "password") {
-          passwordInput.type = "text";
-          toggleVisibilityIcon.innerText = "visibility_off";
-      } else {
-          passwordInput.type = "password";
-          toggleVisibilityIcon.innerText = "visibility";
-      }
-  });
+function generateStrongPassword(length = 12) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}:<>?";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Copy functionality
+    var copyIcon = document.querySelectorAll('.copy');
+    copyIcon.forEach((icon) => {
+        icon.addEventListener('click', function (event) {
+            const input = event.target.parentNode.querySelector('input')
+            navigator.clipboard.writeText(input.value).then(function () {
+                event.target.innerText = "check"
+                setTimeout(() => {
+                    event.target.innerText = 'content_copy'
+                }, 1000)
+            }, function (err) {
+                console.error('Could not copy text: ', err);
+            });
+        });
+    })
+    var toggleVisibilityIcon = document.getElementById('toggleVisibility');
+        toggleVisibilityIcon.addEventListener('click', function () {
+            var passwordInput = document.getElementById('passwordInput');
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                toggleVisibilityIcon.innerText = "visibility_off";
+            } else {
+                passwordInput.type = "password";
+                toggleVisibilityIcon.innerText = "visibility";
+            }
+        });
 });
